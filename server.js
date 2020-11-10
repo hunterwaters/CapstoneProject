900391b8e18b6c4c14d5579eed6afc16be08b52c
hunterwaters <hunterrwaters@gmail.com>
2020-11-10 13:50:44 -0800

    final commit

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/' , (req, res) => res.send('API Running'))
+//Connect to database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false}));
+
+app.get('/' , (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use("/api/users", require('./routes/users'));
+app.use("/api/auth", require('./routes/auth'));
+app.use("/api/profile", require('./routes/profile'));
+app.use("/api/posts", require('./routes/posts'));
 
 const PORT = process.env.PORT || 5000;
 
